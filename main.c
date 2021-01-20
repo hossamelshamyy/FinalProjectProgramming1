e305b5192514e9f2deede8bc93a8a62651b85815
Mina-George137 <minageorge137@gmail.com>
2021-01-20 02:32:31 +0200

    updated main.c student by contact abd added the save and quit functions

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ typedef struct
     char email[40];
     char phone_no[11];
     DOB birth;
-} student;
-student s[100];
+} Contact;
+Contact c[100];
 int count=0;
 void load()
 {
@@ -29,17 +29,19 @@ void load()
     }
     while(!feof(f))
 {
-    fscanf(f,"%[^,],",s[count].firstname);
-        fscanf(f,"%[^,],",s[count].lastname);
-        fscanf(f,"%d-%d-%d,",&s[count].birth.day,&s[count].birth.mon,&s[count].birth.yr);
-        fscanf(f,"%[^,],",s[count].address);
-        fscanf(f,"%[^,],",s[count].phone_no);
-        fscanf(f,"%s",s[count].email);
+    fscanf(f,"%[^,],",c[count].firstname);
+        fscanf(f,"%[^,],",c[count].lastname);
+        fscanf(f,"%d-%d-%d,",&c[count].birth.day,&c[count].birth.mon,&c[count].birth.yr);
+        fscanf(f,"%[^,],",c[count].address);
+        fscanf(f,"%[^,],",c[count].phone_no);
+        fscanf(f,"%s",c[count].email);
         count++;
     }
     fclose(f);
 
 }
+
+
 void query()
 {
     int x,i,j,sum=0;
@@ -48,25 +50,65 @@ void query()
     scanf("%s",&str);
     for(i=0; i<count; i++)
     {
-        if(!strcmp(str,s[i].lastname)){
+        if(!strcmp(str,c[i].lastname)){
 
                 printf("the info of the student:\n");
-                printf("%s  ",s[i].firstname);
-                printf("%s   ",s[i].lastname);
-                printf("%d  ",s[i].birth.day);
-                printf("%d  ",s[i].birth.mon);
-                printf("%d  ",s[i].birth.yr);
-                printf("%s  ",s[i].address);
-                printf("%s  ",s[i].phone_no);
-                printf("%s  ",s[i].email);
+                printf("%s  ",c[i].firstname);
+                printf("%s   ",c[i].lastname);
+                printf("%d  ",c[i].birth.day);
+                printf("%d  ",c[i].birth.mon);
+                printf("%d  ",c[i].birth.yr);
+                printf("%s  ",c[i].address);
+                printf("%s  ",c[i].phone_no);
+                printf("%s  ",c[i].email);
                 printf("\n");
-sum++;
+                sum++;
             }
-        }
+    }
          if(!sum) {
             printf("The last name is not exist :( \n");
            }
-           }
+}
+
+int isSaved = 0;
+int save()
+{
+    FILE*f;
+    f=fopen("Contacts.txt","w");
+   int i;
+   for ( i = 0; i < count; i++)
+   {
+        fprintf(f,"%s,",c[i].firstname);
+        fprintf(f,"%s,",c[i].lastname);
+        fprintf(f,"%d-%d-%d,",&c[i].birth.day,&c[i].birth.mon,&c[i].birth.yr);
+        fprintf(f,"%s,",c[i].address);
+        fprintf(f,"%s,",c[i].phone_no);
+        fprintf(f,"%s\n",c[i].email);      
+   }
+   isSaved=1;
+    fclose(f);
+    return isSaved ;
+}
+
+void quit(){
+    if (isSaved)
+    {
+        exit(0);
+    }else{
+        char answer[2] ;
+        int i;
+        printf("Warning!! your changes will be discarded\n NOTE: answer as follow\n do you want to exit anyways(y/n) ? ");
+        scanf("%s",answer);
+        if (!strcmp(answer , "y"))
+        {
+            exit(0); 
+        }
+        
+        
+    }   
+}
+
+
 
 
 int main()
